221a2a6acaa6e2f0bf41dd951a93d3f53e64b8b4
Vansh Singh Rana <vanshrana602@gmail.com>
2022-09-03 22:09:59 +0530

    further modified the routes and controller

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
-
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
