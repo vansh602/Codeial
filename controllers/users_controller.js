496d356abe27e23ce92317ad5597d34f87d0a70c
Vansh Singh Rana <vanshrana602@gmail.com>
2022-09-15 21:42:26 +0530

    Creating Forms for Sign up Sign In

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,30 @@
 module.exports.profile = function(req, res){
-    res.end('<h1>User Profile</h1>');
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+}
+
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+    //TODO later
+}
+// sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    //TODO later
 }
